44976d60f8a8aa45f125f1af118ea277e4750e75
Lucas <lucas.albergoni@gmail.com>
2021-06-15 19:00:35 -0300

    alteração no sobre

--- portfolio/src/pages/01-sobre.js
+++ portfolio/src/pages/01-sobre.js
@@ -6,14 +6,12 @@ function Sobre() {
 	return (
 		<Container fluid className='part' id='sobre'>
 			<h1>Olá</h1>
-			<h2>Me chamo Lucas Albergoni</h2>
+			<h2>Sou Lucas Albergoni</h2>
 			<p>
 				Sou Desenvolvedor Web Front-End.
 				<br />
-				Graduado desde 2018 em Sistemas para Internet e atualmente trabalhando principalmente com
-				ReactJS.
-				<br />
-				Possuo pós graduação em nível de especialista em Banco de dados.
+				Formado em Sistemas para Internet e Especialista em Banco de dados. Atualmente trabalho
+				principalmente com ReactJS .
 			</p>
 			<Button href={Curriculum} type='button'>
 				Baixar Curriculum
